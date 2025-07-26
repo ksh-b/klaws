0a397a53cbb28e842fe7c420118e7351d5d07fb9
kshib <ksyko@pm.me>
2025-07-26 14:15:57 +0530

    change readability library

--- lib/provider/extractor/readability.dart
+++ lib/provider/extractor/readability.dart
@@ -1,7 +1,8 @@
 import 'package:dio/dio.dart';
+import 'package:html/parser.dart';
 import 'package:klaws/model/article.dart';
 import 'package:klaws/model/publisher.dart';
-import 'package:readability/readability.dart' as readability;
+import 'package:xayn_readability_core/xayn_readability_core.dart' as readability;
 
 Future<Article> extractArticle(
   Source source,
@@ -11,19 +12,21 @@ Future<Article> extractArticle(
   if(source.nest!=null && source.nest!.headers.json_.isNotEmpty) {
     dio.options = BaseOptions(headers: source.nest!.headers.json_);
   }
-  var fullArticle = await readability.parseAsync(article.url);
+  var response = await dio.get(article.url);
+  var html = parse(response);
+  var fullArticle = readability.parse(html);
   return Article(
     source: article.source,
     sourceName: article.sourceName,
-    title: fullArticle.title ?? article.title,
-    content: fullArticle.content ?? article.content,
-    excerpt: fullArticle.excerpt ?? article.excerpt,
-    author: fullArticle.author ?? article.author,
+    title: fullArticle?.metadata.title ?? article.title,
+    content: fullArticle?.html ?? html.body?.innerHtml ?? article.content,
+    excerpt: fullArticle?.metadata.excerpt ?? article.excerpt,
+    author: fullArticle?.metadata.byline ?? article.author,
     url: article.url,
-    thumbnail: fullArticle.imageUrl ?? article.thumbnail,
+    thumbnail: article.thumbnail,
     category: article.category,
     tags: article.tags,
     publishedAt: article.publishedAt,
-    publishedAtString: fullArticle.publishedTime ?? article.publishedAtString,
+    publishedAtString: article.publishedAtString,
   );
 }
