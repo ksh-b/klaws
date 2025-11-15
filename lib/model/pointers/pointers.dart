899db8f6fae36b35ec31bc2dca727283ceab3b52
kshib <ksyko@pm.me>
2025-11-16 00:17:54 +0530

    remove description and changelog

--- lib/model/pointers/pointers.dart
+++ lib/model/pointers/pointers.dart
@@ -3,23 +3,17 @@ import 'package:klaws/model/pointers/watch_pointer.dart';
 
 class Pointers {
   String name = "";
-  String description = "";
-  String changelog = "";
   List<PublisherPointer> publisherPointers = [];
   List<WatchPointer> watchPointers = [];
 
   Pointers({
     required this.name,
-    required this.description,
-    required this.changelog,
     required this.publisherPointers,
     required this.watchPointers,
   });
 
   Pointers.fromJson(dynamic json_) {
     name = json_['name'];
-    description = json_['description'];
-    changelog = json_['changelog'];
     if (json_['publishers'] != null) {
       publisherPointers = [];
       json_['publishers'].forEach((v) {
@@ -37,8 +31,6 @@ class Pointers {
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     map['name'] = name;
-    map['description'] = description;
-    map['changelog'] = changelog;
     map['publishers'] = publisherPointers.map((v) => v.toJson()).toList();
       return map;
   }
